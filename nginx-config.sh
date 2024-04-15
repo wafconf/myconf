6858784d123c2227622bbcd3c03fc824cc81eb46
wafconf <167112315+wafconf@users.noreply.github.com>
2024-04-16 00:00:13 +0700

    Update nginx-config.sh

--- nginx-config.sh
+++ nginx-config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # URL ke raw file di GitHub
-FILE_URL1="https://raw.githubusercontent.com/wafconf/myconf/main/block-user-agent.conf"
-FILE_URL2="https://raw.githubusercontent.com/wafconf/myconf/main/bad-referrer.conf"
+FILE_URL1="https://raw.githubusercontent.com/username/repo/main/path/to/block-user-agent.conf"
+FILE_URL2="https://raw.githubusercontent.com/username/repo/main/path/to/bad-referrer.conf"
 
 # Lokasi di mana file akan disimpan
 DESTINATION1="/etc/nginx/block-user-agent.conf"
@@ -10,9 +10,6 @@ DESTINATION2="/etc/nginx/bad-referrer.conf"
 
 # Lokasi file konfigurasi nginx
 NGINX_CONF="/etc/nginx/nginx.conf"
-
-# Memberikan izin eksekusi kepada skrip ini
-chmod +x "$0"
 
 # Cek apakah script dijalankan sebagai root
 if [ "$(id -u)" != "0" ]; then
@@ -29,19 +26,16 @@ echo "bad-referrer.conf has been downloaded and saved to $DESTINATION2."
 # Backup file konfigurasi nginx sebelum modifikasi
 cp $NGINX_CONF "${NGINX_CONF}.bak"
 
-# Mencari blok 'http' untuk memasukkan konfigurasi map sebelumnya
-HTTP_BLOCK_START_LINE=$(grep -n "^http {" $NGINX_CONF | cut -d : -f 1)
-
-# Menambahkan map block untuk bad bots jika belum ada
-if ! grep -q "map \$http_user_agent \$bad_bot" $NGINX_CONF; then
-    sed -i "${HTTP_BLOCK_START_LINE}i\\\nmap \$http_user_agent \$bad_bot {\n    default 0;\n    include /etc/nginx/block-user-agent.conf;\n}\n" $NGINX_CONF
-fi
-
-# Menambahkan map block untuk bad referers jika belum ada
-if ! grep -q "map \$http_referer \$bad_referer" $NGINX_CONF; then
-    sed -i "${HTTP_BLOCK_START_LINE}i\\\nmap \$http_referer \$bad_referer {\n    default 0;\n    include /etc/nginx/bad-referrer.conf;\n}\n" $NGINX_CONF
-fi
+# Menambahkan map block ke nginx.conf dalam blok http
+sed -i '/http {/ a \\n    # Map blocks\n    map $http_user_agent $bad_bot {\n        default 0;\n        include /etc/nginx/block-user-agent.conf;\n    }\n\n    map $http_referer $bad_referer {\n        default 0;\n        include /etc/nginx/bad-referrer.conf;\n    }\n' $NGINX_CONF
 
 # Memeriksa dan mengulang konfigurasi nginx untuk memastikan tidak ada kesalahan
 nginx -t && systemctl reload nginx
-echo "Nginx configuration has been updated and reloaded."
+if [ $? -eq 0 ]; then
+    echo "Nginx configuration has been updated and reloaded."
+else
+    echo "Failed to update Nginx configuration."
+    # Restore the original configuration file
+    mv "${NGINX_CONF}.bak" $NGINX_CONF
+    exit 1
+fi
